5906ebc34c838492bbfd11cb15c929fc61506d36
bryanu <bryanutley2000@yahoo.com>
2017-02-06 11:55:46 -0600

    Feb-2-2017a

---
+++ Bryan_Utley/fundamentals/arrays.rb
@@ -0,0 +1,53 @@
+# fetch
+#puts; print "fetch"; print "*" * 50; puts
+# h = { "a" => 100, "b" => 200 }
+# puts h.fetch("a")                          
+# puts h.fetch("z", "Not found!")                 
+
+
+# delete
+# puts; print "*" * 50; puts
+# arr = [10,20,30,40]
+# arr.delete(20)
+# puts arr
+
+# reverse
+# arr = ["Cool", "is", "Bryan"]
+# arr.reverse!
+# puts arr
+
+# length
+# str = "Bryan is Cool"
+# puts "Length of string '#{str}'is #{str.length} characters long."
+
+# sort
+# arr = [20,10,40,30,5]
+# arr.sort!
+# print arr
+
+#slice
+# arr = ["Cool", "is", "Bryan"]
+# arr.reverse!
+# arr.slice!(1)
+# print arr
+
+#shuffle
+# arr = ["Hearts", "Clubs", "Diamonds", "Hearts"]
+# print arr
+# puts
+# arr.shuffle!
+# print arr
+
+#join
+# arr = ["Hearts", "Clubs", "Diamonds", "Hearts"]
+# str = arr.join(" | ")
+# puts str
+
+#insert
+# arr = ["Hearts", "Diamonds", "Hearts"]
+# arr.insert(1, "Clubs")
+# puts arr
+
+#values_at()
+# a = ["cat", "dog", "bear"]
+# puts a.values_at(1,2).join(' and ') 
