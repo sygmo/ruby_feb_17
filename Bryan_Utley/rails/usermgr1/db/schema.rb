6d2c559d1dc650be59b8e0a8b8aa6298282f6e59
bryanu <bryanutley2000@yahoo.com>
2017-02-14 15:51:43 -0600

    Feb-14-2017c

--- Bryan_Utley/rails/usermgr1/db/schema.rb
+++ Bryan_Utley/rails/usermgr1/db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213223928) do
+ActiveRecord::Schema.define(version: 20170214212209) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.string   "password"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
