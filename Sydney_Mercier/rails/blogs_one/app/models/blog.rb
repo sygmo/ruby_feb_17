d6999e2147ab30d1f044d7497fea6c1ae99841a4
sygmo <sydney.mercier91@gmail.com>
2017-02-15 13:54:17 -0600

    blogs/posts/messages I

---
+++ Sydney_Mercier/rails/blogs_one/app/models/blog.rb
@@ -0,0 +1,5 @@
+class Blog < ActiveRecord::Base
+	has_many :posts, dependent: :destroy, { includes :messages }
+
+	validates :name, :description, presence: true
+end
---
+++ Sydney_Mercier/rails/blogs_one/app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ActiveRecord::Base
+	belongs_to :post
+
+	validates :author, presence: true
+	validates :message, presence: true, length: { minimum: 15 }
+end
