73cc4bce0e923e75b16d9fcbee89567dfc86902f
Chris Ulanowicz <src3collector@gmail.com>
2017-02-17 10:16:09 -0600

    added RSpec routes demo

---
+++ Chris_Ulanowicz/demos/RSpec_routes/dojos_controller.rb
@@ -0,0 +1,16 @@
+# this file is normally located in app/controllers
+
+class DojosController < ApplicationController
+	def index
+		
+	end
+	def world
+		
+	end
+	def ninjas
+		
+	end
+	def create
+		
+	end
+end
